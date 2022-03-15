334a6a4ae0e2583dd7086e8bdf7b92c3e9362050
saikiran <neo2603@gmail.com>
2022-03-15 13:28:47 +0530

    correcting license and bug tracker path

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/saikiran2603/k8s-scheduler/",
     project_urls={
-        "Bug Tracker": "https://github.com/saikiran2603/k8s-scheduler//issues",
+        "Bug Tracker": "https://github.com/saikiran2603/k8s-scheduler/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
